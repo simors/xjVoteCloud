dfcc0884a5a4d24696c4bf2fa4cfada892e9115e
yang24201 <yang24201@126.com>
2017-11-14 17:42:56 +0800

    add update user method

--- cloud.js
+++ cloud.js
@@ -1,8 +1,7 @@
 var AV = require('leanengine');
+import * as userCloud from './cloud/user'
 
 /**
  * 一个简单的云代码方法
  */
-AV.Cloud.define('hello', function(request) {
-  return 'Hello world!';
-});
+AV.Cloud.define('userUpdateInfo', userCloud.updateUserInfo);
--- cloud/user/index.js
+++ cloud/user/index.js
@@ -1,3 +1,29 @@
 /**
  * Created by yangyang on 2017/11/14.
  */
+import AV from 'leanengine'
+
+export async function updateUserInfo(request) {
+  let {nickname, gender, avatar, province, city} = request.params
+  let currentUser = request.currentUser
+  if (!currentUser) {
+    throw new AV.Cloud.Error('Permission denied, need to login first', {code: 100});
+  }
+
+  if (nickname) {
+    currentUser.set('nickname', nickname)
+  }
+  if (gender) {
+    currentUser.set('gender', gender)
+  }
+  if (avatar) {
+    currentUser.set('avatar', avatar)
+  }
+  if (province) {
+    currentUser.set('province', province)
+  }
+  if (city) {
+    currentUser.set('city', city)
+  }
+  return await currentUser.save()
+}
