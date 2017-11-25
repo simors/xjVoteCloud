21d905e86d9827f6328d8eafc436b8341a3bd180
yang24201 <yang24201@126.com>
2017-11-26 00:01:27 +0800

    add create or update vote method

--- cloud.js
+++ cloud.js
@@ -8,3 +8,4 @@ AV.Cloud.define('userUpdateInfo', userCloud.updateUserInfo);
 /* 投票 */
 AV.Cloud.define('voteFetchGifts', voteCloud.fetchGifts);
 AV.Cloud.define('voteCreateVote', voteCloud.createVote);
+AV.Cloud.define('voteCreateOrUpdateVote', voteCloud.createOrUpdateVote);
--- cloud/vote/index.js
+++ cloud/vote/index.js
@@ -4,6 +4,14 @@
 import AV from 'leanengine'
 import * as errno from '../errno'
 
+const VOTE_STATUS = {
+  EDITING: 1,     // 正在编辑
+  PAYING: 2,      // 待支付
+  WAITING: 3,     // 未开始
+  STARTING: 4,    // 正在进行
+  DONE: 5,        // 已结束
+}
+
 function constructGift(leanAward) {
   let award = {}
   if (!leanAward) {
@@ -58,5 +66,104 @@ export async function createVote(request) {
   vote.set('startDate', startDate)
   vote.set('expire', expire)
   vote.set('creator', currentUser)
+  vote.set('status', VOTE_STATUS.EDITING)
   return await vote.save()
+}
+
+async function newVote(voteObj) {
+  let Votes = AV.Object.extend('Votes')
+  let vote = new Votes()
+  vote.set('title', voteObj.title)
+  vote.set('cover', voteObj.cover)
+  vote.set('notice', voteObj.notice)
+  vote.set('rule', voteObj.rule)
+  vote.set('organizer', voteObj.organizer)
+  vote.set('awards', voteObj.awards)
+  vote.set('gifts', voteObj.gifts)
+  vote.set('startDate', voteObj.startDate)
+  vote.set('expire', voteObj.expire)
+  vote.set('creator', voteObj.user)
+  vote.set('status', VOTE_STATUS.EDITING)
+  return await vote.save()
+}
+
+async function updateVote(voteObj) {
+  let vote = AV.Object.createWithoutData('Votes', voteObj.id)
+  if (voteObj.title) {
+    vote.set('title', voteObj.title)
+  }
+  if (voteObj.cover) {
+    vote.set('cover', voteObj.cover)
+  }
+  if (voteObj.notice) {
+    vote.set('notice', voteObj.notice)
+  }
+  if (voteObj.rule) {
+    vote.set('rule', voteObj.rule)
+  }
+  if (voteObj.organizer) {
+    vote.set('organizer', voteObj.organizer)
+  }
+  if (voteObj.awards) {
+    vote.set('awards', voteObj.awards)
+  }
+  if (voteObj.gifts) {
+    vote.set('gifts', voteObj.gifts)
+  }
+  if (voteObj.startDate) {
+    vote.set('startDate', voteObj.startDate)
+  }
+  if (voteObj.expire) {
+    vote.set('expire', voteObj.expire)
+  }
+  if (voteObj.status) {
+    vote.set('status', voteObj.status)
+  }
+  return await vote.save()
+}
+
+/**
+ * 创建或更新投票活动，当判断没有id时，创建一个新的投票，否则更新已有投票
+ * @param request
+ * @returns {*}
+ */
+export async function createOrUpdateVote(request) {
+  let currentUser = request.currentUser
+  if (!currentUser) {
+    throw new AV.Cloud.Error('Permission denied, need to login first', {code: errno.EACCES});
+  }
+  let {id, title, cover, notice, rule, organizer, awards, gifts, startDate, expire, status} = request.params
+  let voteObj = undefined
+  let result = undefined
+  if (!id) {
+    voteObj = {
+      user: currentUser,
+      title,
+      cover,
+      notice,
+      rule,
+      organizer,
+      awards,
+      gifts,
+      startDate,
+      expire
+    }
+    result = await newVote(voteObj)
+    return result
+  }
+  voteObj = {
+    id,
+    title,
+    cover,
+    notice,
+    rule,
+    organizer,
+    awards,
+    gifts,
+    startDate,
+    expire,
+    status
+  }
+  result = await updateVote(voteObj)
+  return result
 }
