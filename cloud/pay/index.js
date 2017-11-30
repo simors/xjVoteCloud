a05050041b39a97c7dcf141b5304be026ba47cce
wanpeng <wanpeng19860919@163.com>
2017-11-30 17:43:28 +0800

    <feat>接入微信支付

---
+++ cloud/pay/index.js
@@ -0,0 +1,69 @@
+/**
+ * Created by wanpeng on 2017/11/30.
+ */
+import AV from 'leancloud-storage'
+import * as errno from '../errno'
+import mathjs from 'mathjs'
+import Pingpp from 'pingpp'
+import uuidv4 from 'uuid'
+import Promise from 'bluebird'
+
+var pingpp = Pingpp(process.env.PINGPP_API_KEY)
+
+export async function createPaymentRequest(request) {
+  const {currentUser, meta} = request
+  if (!currentUser) {
+    throw new AV.Cloud.Error('Permission denied, need to login first', {code: errno.EACCES})
+  }
+  const remoteAddress = meta.remoteAddress
+  const {amount, channel, metadata, openid, subject} = request.params
+  pingpp.setPrivateKeyPath(__dirname + "/rsa_private_key.pem")
+
+  try {
+    const charges = await new Promise((resolve, reject) => {
+      const order_no = uuidv4().replace(/-/g, '').substr(0, 16)
+      pingpp.charges.create({
+        order_no: order_no,
+        app: {id: process.env.PINGPP_APP_ID},
+        channel: channel,
+        amount: amount,
+        client_ip: remoteAddress,
+        currency: "cny",
+        subject: subject,
+        body: "商品的描述信息",
+        extra: {
+          open_id: openid
+        },
+        description: "",
+        metadata: metadata,
+      }, function (err, charge) {
+        if (err != null) {
+          reject(new AV.Cloud.Error('request charges error' + err.message, {code: errno.ERROR_CREATE_CHARGES}))
+        }
+        resolve(charge)
+      })
+    })
+    return charges
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function createWithdrawRequest(request) {
+  const {currentUser} = request
+  if (!currentUser) {
+    throw new AV.Cloud.Error('Permission denied, need to login first', {code: errno.EACCES})
+  }
+
+  const {} = request.params
+}
+
+export async function handlePaymentWebhootsEvent(request) {
+  const {} = request.params
+
+}
+
+export async function handleWithdrawWebhootsEvent(request) {
+  const {} = request.params
+
+}
